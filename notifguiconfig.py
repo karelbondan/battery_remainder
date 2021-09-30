68cab4ecfaa75ffce8f9546f8d6f5f9950ab17cf
karelbondan <karel.herdito@binus.ac.id>
2021-10-01 00:24:25 +0700

    configuration for notification GUI

---
+++ notifguiconfig.py
@@ -0,0 +1,21 @@
+from PySide2.QtWidgets import QGraphicsDropShadowEffect
+
+from main import *
+
+# the function to drag the window around when pressed on the title bar, and to make the
+# minimize and close button works.
+class UIFunctions(MainWindow):
+    def removeTitle(self):
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+        self.shadow = QGraphicsDropShadowEffect(self)
+        self.shadow.setBlurRadius(20)
+        self.shadow.setXOffset(0)
+        self.shadow.setYOffset(0)
+        self.shadow.setColor(QColor(0, 0, 0, 100))
+
+        self.ui.body.setGraphicsEffect(self.shadow)
+
+        # closes the app when the close button is pressed
+        self.ui.close.clicked.connect(lambda: self.close())
